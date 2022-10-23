2517e6c4d91df4751020540936c8580fa7e9407f
Ekaterina Skirdina <katyonok.ya17@gmail.com>
2022-10-23 10:45:02 +0300

    Task 41

---
+++ Task41/Program.cs
@@ -0,0 +1,39 @@
+﻿/* Задача 41. Пользователь вводит с клавиатуры М чисел.
+Пoсчитайте, сколько чисел больше 0 ввёл пользователь.*/
+
+void Main()
+{
+    int i = GetAmountOfInput();
+    int count = CountPositiveNumbers(i);
+    OutPut(count);
+}
+
+int GetAmountOfInput()
+{
+    Console.WriteLine("Введите колличество чисел: ");
+    int amount = int.Parse(Console.ReadLine());
+    return amount;
+}
+
+int CountPositiveNumbers(int inputQuantity)
+{
+    int countPosNum = 0;
+    for (int i = 0; i < inputQuantity; i++)
+    {
+        Console.WriteLine($"Введите число{i + 1}: ");
+        int number = int.Parse(Console.ReadLine());
+        if (number > 0)
+        {
+            countPosNum++;
+        }
+    }
+    return countPosNum;
+}
+
+void OutPut(int arg)
+{
+    Console.WriteLine();
+    Console.WriteLine(arg);
+}
+
+Main();
